adbce6008363d0b5762fef20d91e23742cac92f5
RRR_RRR <97770403+dairuru@users.noreply.github.com>
2026-03-03 18:44:26 -0500

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -11,7 +11,7 @@ infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
 '''
 
 def connect_to_eth():
-	url = f"https://mainnet.infura.io/v3/{f90904de1f944093b688120e1b698dcb}"  # FILL THIS IN
+	url = f"https://mainnet.infura.io/v3/f90904de1f944093b688120e1b698dcb"  # FILL THIS IN
 	w3 = Web3(HTTPProvider(url))
 	assert w3.is_connected(), f"Failed to connect to provider at {url}"
 	return w3
